ae205cd9805efc720207e7f4a8edfa56b59aba0b
vtullo03 <vitotullo430@gmail.com>
2023-09-30 20:59:27 -0400

    re-organizing + cleanup

--- HW1/HW1/main.cpp
+++ HW1/HW1/main.cpp
@@ -25,36 +25,32 @@
 
 #define LOG(argument) std::cout << argument << '\n'
 
-// Our window dimensions
+// window dimensions + viewport
 const int WINDOW_WIDTH = 640,
           WINDOW_HEIGHT = 480;
 
-// Background color components
-const float BG_RED = 0.404f,
-            BG_BLUE = 0.016f,
-            BG_GREEN = 0.016f,
-            BG_OPACITY = 1.0f;
-
-// Our viewport—or our "camera"'s—position and dimensions
 const int VIEWPORT_X = 0,
           VIEWPORT_Y = 0,
           VIEWPORT_WIDTH = WINDOW_WIDTH,
           VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
+// Background color -- blood red
+const float BG_RED = 0.404f,
+            BG_BLUE = 0.016f,
+            BG_GREEN = 0.016f,
+            BG_OPACITY = 1.0f;
+
+// shaders
 const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
            F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
-// Our object's fill colour
-const float TRIANGLE_RED = 1.0,
-            TRIANGLE_BLUE = 0.4,
-            TRIANGLE_GREEN = 0.4,
-            TRIANGLE_OPACITY = 1.0;
+ShaderProgram g_shader_program;
 
+// to display game and check if running
 bool g_game_is_running = true;
 SDL_Window* g_display_window;
 
-ShaderProgram g_shader_program;
-
+// VVVVV ALL MATRIXES VVVVV
 glm::mat4 g_view_matrix,        // position of the camera
 g_model_matrix,       // transformations of the object 
 g_model_matrix_leftwing,
@@ -63,15 +59,16 @@ g_model_matrix_leftglow,
 g_model_matrix_rightglow,
 g_projection_matrix;  // characteristic of the camera
 
-// keeps track of the frames
-float g_frame_counter = 0.0f;
+float g_frame_counter = 0.0f; // keeps track of the frames
+// change speed constants
 const float MAX_FRAME = 4;
 const float MOVEMENT_SPEED = 1.0f;
+const float GROWTH_FACTOR = .3f;
 
 // for time.deltaTime
 float g_previous_ticks = 0.0f;
 
-// texture variables
+// TEXTURE VARIABLES
 const char GABRIEL_SPRITE[] = "GabrielBaseBIG.png",
            LEFT_WING_SPRITE[] = "GabrielWingLeftBIG.png",
            RIGHT_WING_SPRITE[] = "GabrielWingRightBIG.png",
@@ -88,14 +85,14 @@ const int NUMBER_OF_TEXTURES = 1;
 const GLint LEVEL_OF_DETAIL = 0;
 const GLint TEXTURE_BORDER = 0;
 
+// keeps track of transformations
 float x_movement = -3.0f,
     y_movement = -2.0f,
     left_flap = 0.0f,
-    right_flap = 0.0f;
+    right_flap = 0.0f,
+    growth = 0.0f;
 
-const float GROWTH_FACTOR = .3f;
-float growth = 0.0f;
-
+// loads a texture to be used by OpenGL
 GLuint load_texture(const char* filepath)
 {
     // STEP 1: Loading the image file
@@ -124,8 +121,10 @@ GLuint load_texture(const char* filepath)
     return textureID;
 }
 
+// initialises the game -- ONLY RUN ONCE AT START
 void initialise()
 {
+    // create window
     SDL_Init(SDL_INIT_VIDEO);
     g_display_window = SDL_CreateWindow("HW 1!!!!",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -135,6 +134,7 @@ void initialise()
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
 
+    // for windows machines
 #ifdef _WINDOWS
     glewInit();
 #endif
@@ -143,6 +143,7 @@ void initialise()
 
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
 
+    // initializes all the model matrixes
     g_model_matrix = glm::mat4(1.0f);
     g_model_matrix_leftwing = glm::mat4(1.0f);
     g_model_matrix_rightwing = glm::mat4(1.0f);
@@ -159,6 +160,7 @@ void initialise()
 
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
 
+    // load the textures with the images 
     gabriel_texture_id = load_texture(GABRIEL_SPRITE);
     left_wing_texture_id = load_texture(LEFT_WING_SPRITE);
     right_wing_texture_id = load_texture(RIGHT_WING_SPRITE);
@@ -180,6 +182,7 @@ void initialise()
 void process_input()
 {
     SDL_Event event;
+    // check if game is quit
     while (SDL_PollEvent(&event))
     {
         if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
@@ -191,13 +194,14 @@ void process_input()
 
 void move_in_v(glm::mat4& object_model_matrix, float delta_time)
 {
+    // reset matrix
     object_model_matrix = glm::mat4(1.0f);
 
     // account for delta time
     float speed = delta_time * MOVEMENT_SPEED;
 
     // moves object in a upside down V motion
-    // breaks the movements into 8 segments
+    // breaks the movements into 4 segments
     if (g_frame_counter < (MAX_FRAME / 4))
     {
         x_movement += delta_time * MOVEMENT_SPEED;
@@ -221,19 +225,8 @@ void move_in_v(glm::mat4& object_model_matrix, float delta_time)
     object_model_matrix = glm::translate(object_model_matrix, glm::vec3(x_movement, y_movement, 0.0f));
 }
 
-void wing_flap(glm::mat4& object_model_matrix, float delta_time, float& flap_speed, float& flap)
-{
-    if (g_frame_counter < (MAX_FRAME / 2))
-    {
-        object_model_matrix = glm::rotate(object_model_matrix, glm::radians(flap_speed * delta_time), glm::vec3(0.0f, 0.0f, 1.0f));
-    }
-    if (g_frame_counter > (MAX_FRAME / 2))
-    {
-        object_model_matrix = glm::rotate(object_model_matrix, glm::radians(-flap_speed * delta_time), glm::vec3(0.0f, 0.0f, 1.0f));
-    }
-    if (g_frame_counter >= MAX_FRAME) g_frame_counter = 0;
-}
-
+// pulses the transparent textures
+// simulates a glowing effect
 void pulse(glm::mat4& object_model_matrix, float delta_time)
 {
     if (g_frame_counter < MAX_FRAME / 2)
@@ -256,6 +249,7 @@ void update()
     g_frame_counter += delta_time;
     g_previous_ticks = ticks;
 
+    // translate body and left wing
     move_in_v(g_model_matrix, delta_time);
     move_in_v(g_model_matrix_leftwing, delta_time);
     move_in_v(g_model_matrix_leftglow, delta_time);
@@ -267,10 +261,12 @@ void update()
     {
         left_flap -= 20.0f * delta_time;
     }
-    g_model_matrix_leftwing = glm::rotate(g_model_matrix_leftwing, glm::radians(left_flap), glm::vec3(0.0f, 0.0f, 1.0f));
+    g_model_matrix_leftwing = glm::rotate(g_model_matrix_leftwing, glm::radians(left_flap), glm::vec3(0.0f, 0.0f, 1.0f)); // rotate wing
     pulse(g_model_matrix_leftglow, delta_time);
-    g_model_matrix_leftglow = glm::rotate(g_model_matrix_leftglow, glm::radians(left_flap), glm::vec3(0.0f, 0.0f, 1.0f));
+    g_model_matrix_leftglow = glm::rotate(g_model_matrix_leftglow, glm::radians(left_flap), glm::vec3(0.0f, 0.0f, 1.0f)); 
+    // pulse flow and THEN move with wing
 
+    // apply same to right wing
     move_in_v(g_model_matrix_rightwing, delta_time);
     move_in_v(g_model_matrix_rightglow, delta_time);
     if(g_frame_counter < (MAX_FRAME / 2))
@@ -317,20 +313,21 @@ void render() {
     glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
     glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
-    // Bind texture
+    // Bind textures
     draw_object(g_model_matrix, gabriel_texture_id);
     draw_object(g_model_matrix_leftwing, left_wing_texture_id);
     draw_object(g_model_matrix_rightwing, right_wing_texture_id);
     draw_object(g_model_matrix_leftglow, left_glow_texture_id);
     draw_object(g_model_matrix_rightglow, right_glow_texture_id);
 
-    // We disable two attribute arrays now
+    // Disable
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
     glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
     SDL_GL_SwapWindow(g_display_window);
 }
 
+// shutdown safely
 void shutdown() { SDL_Quit(); }
 
 int main(int argc, char* argv[])
